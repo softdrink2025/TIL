8f75837ac13199eae7ec5d64980d0571e0f3ce94
thead97 <thead97@naver.com>
2025-09-17 17:54:56 +0900

    자리이동

--- 알고리즘/try.py
+++ 알고리즘/try.py
@@ -1,15 +1,40 @@
-N = 6
-A = [i for i in range(1, N+1)]
+# import sys
+# sys.stdin = open('input.txt')
 
-key = 10
-bit = [0] * N
 
-def f(i, N):  # bit[i]를 결정하는 함수
-    if i == N:
-        print(bit)
-    else:
-        bit[i] = 1
-        f(i + 1, N)
-        bit[i] = 0
-        f(i + 1, N)
-f(0, N)
+def search(ci, cj, pi, pj, distance, cnt):  # 현재 위치와 이전 위치, 이동 거리, 수확한 사과 수
+    global result
+
+    distance += abs(ci - pi) + abs(cj - pj)
+
+    if result <= distance:
+        return
+
+    if cnt == N:  # 모든 사과를 수확했다면
+        distance += abs(ci - 0) + abs(cj - 0)
+        if result > distance:
+            result = distance
+        return
+
+    for i in range(N):
+        if not visited[i]:
+            visited[i] = True
+            search(apples[i][0], apples[i][1], ci, cj, distance, cnt + 1)
+            visited[i] = False
+
+
+T = int(input())
+for tc in range(1, T + 1):
+    N = int(input())
+    apples = [list(map(int, input().split())) for _ in range(N)]
+    visited = [False] * N
+    result = 200 * 10
+    search(0, 0, 0, 0, 0, 0)
+    print(result)
+
+
+
+
+
+
+
