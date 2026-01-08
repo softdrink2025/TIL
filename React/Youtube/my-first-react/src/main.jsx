3299d8c7418250fa6132e02e8ce41a4f131ff396
softdrink2025 <softdrink2025@gmail.com>
2026-01-08 22:20:17 +0900

    2026/01/08 React 유튜브 강의

--- React/Youtube/my-first-react/src/main.jsx
+++ React/Youtube/my-first-react/src/main.jsx
@@ -1,14 +1,35 @@
-import { createRoot } from 'react-dom/client';
+import { useState } from "react";
+import { createRoot } from 'react-dom/client'
 
 function App() {
-  const sayHi = (name, e) => {
-    alert(`${name}님, 안녕하세요.`);
-    alert(`발생한 이벤트는 ${e.type}입니다.`);
+  const [flower, setFlower] = useState("장미꽃");
+
+  const handleChange = (e) => {
+    setFlower(e.target.value);
   }
 
-  return (
-    <button onDoubleClick={(event) => sayHi("홍길동", event)}>인사 버튼</button>
-  )
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let result = `당신이 고른 꽃은 ${flower} 입니다.`
+    alert(result);
+  }
+
+  return ( 
+    <form onSubmit={handleSubmit}>
+      <label>꽃을 골라 주세요.</label>
+      <br />
+      <label>
+        <input type="radio" name="flower" value="장미꽃" checked={flower == "장미꽃"} onChange={handleChange} /> 장미꽃
+      </label>
+      <label>
+        <input type="radio" name="flower" value="국화" checked={flower == "국화"} onChange={handleChange} /> 국화
+      </label>
+      <label>
+        <input type="radio" name="flower" value="코스모스" checked={flower == "코스모스"} onChange={handleChange} /> 코스모스
+      </label>
+      <button>Submit</button>
+    </form>
+  );
 }
 
 createRoot(document.getElementById('root')).render(
