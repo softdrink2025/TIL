a652fc26ba31158f5de25aa6a946c5bd825c6016
softdrink2025 <softdrink2025@gmail.com>
2026-01-14 00:20:55 +0900

    2026/01/14 React 유튜브 강의

--- React/Youtube/tw-todos/src/App.tsx
+++ React/Youtube/tw-todos/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import TodoItem from "./components/TodoItem"
 import { dummyData } from "./data/todos"
+import AddTodoForm from "./components/AddTodoForm";
 
 function App() {
 
@@ -12,10 +13,23 @@ function App() {
     ))
   }
 
+  function addTodo(title:string) {
+    setTodos(
+      (prevTodos) => [
+        {
+          id: prevTodos.length + 1,
+          title,
+          completed:false
+        },
+        ...prevTodos,
+      ])
+  }
+
   return (
     <main className="py-8 h-screen space-y-5">
       <h1 className="font-bold text-3xl text-center">나의 일정</h1>
-      <div className="max-lg mx-auto bg-slate-100 rounded-md p-5">
+      <div className="max-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6">
+        <AddTodoForm onSubmit={addTodo} />
         <div className="space-y-2">
           {todos.map(todo => (
             <TodoItem
